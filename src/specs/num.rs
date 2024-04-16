20efcd8d0b7a83d15c49c0913891154a6a8e4a33
Lucas Franceschino <lucas@cryspen.com>
2024-04-16 09:33:55 +0200

    feat: num: derive for more unsigned integers

--- src/specs/num.rs
+++ src/specs/num.rs
@@ -1,4 +1,95 @@
 use crate::helpers::*;
+use paste::paste;
+
+macro_rules! mk_unsigned_tests {
+    ($typ:ident, $ident:ident) => {
+        mk_unsigned_tests!($typ, $ident, $typ::MAX);
+    };
+    ($typ:ident, $ident:ident, $max:expr) => {
+        paste! {
+            tests! {
+                header: concat!("Properties for [`core::ops::Add::<", stringify!($typ), ">::add`]"),
+                ident: [<core_ops_add_ $typ _add>],
+                contract! {
+                    header: "Semantics of non-overflowing addition",
+                    inputs: [x: $typ, y: $typ],
+                    precondition: x.up() + y.up() <= $max.up(),
+                    postcondition: x + y == eval($typ::down(x.up() + y.up())),
+                },
+                contract! {
+                    header: "Panics when overflowing",
+                    inputs: [x: $typ, y: $typ],
+                    precondition: x.up() + y.up() > $max.up(),
+                    postcondition: panics!(x + y),
+                },
+                contract! {
+                    header: "Commutativity",
+                    inputs: [x: $typ, y: $typ],
+                    precondition: x.up() + y.up() <= $max.up(),
+                    postcondition: x + y == y + x,
+                },
+                contract! {
+                    header: "Left identity",
+                    inputs: [x: $typ],
+                    precondition: true,
+                    postcondition: x + 0 == x,
+                },
+                contract! {
+                    header: "Right identity",
+                    inputs: [x: $typ],
+                    precondition: true,
+                    postcondition: 0 + x == x,
+                },
+                contract! {
+                    header: "Associativity",
+                    inputs: [x: $typ, y: $typ, z: $typ],
+                    precondition: x.up() + y.up() + z.up() <= $max.up(),
+                    postcondition: (x + y) + z == x + (y + z),
+                },
+            }
+            tests! {
+                header: concat!("Properties for [`", stringify!($typ), "::checked_add`]"),
+                ident: [<core_ops_add_ $typ _checked_add>],
+                contract! {
+                    header: "Semantics of non-overflowing checked addition",
+                    inputs: [x: $typ, y: $typ],
+                    precondition: x.up() + y.up() <= $max.up(),
+                    postcondition: x.checked_add(y) == Some(eval($typ::down(x.up() + y.up()))),
+                },
+                contract! {
+                    header: "None when overflowing",
+                    inputs: [x: $typ, y: $typ],
+                    precondition: x.up() + y.up() > $max.up(),
+                    postcondition: x.checked_add(y) == None,
+                },
+                contract! {
+                    header: "Commutativity",
+                    inputs: [x: $typ, y: $typ],
+                    precondition: true,
+                    postcondition: x.checked_add(y) == y.checked_add(x),
+                },
+                contract! {
+                    header: "Left identity",
+                    inputs: [x: $typ],
+                    precondition: true,
+                    postcondition: x.checked_add([<0 $typ>]) == Some(x),
+                },
+                contract! {
+                    header: "Right identity",
+                    inputs: [x: $typ],
+                    precondition: true,
+                    postcondition: [<0 $typ>].checked_add(x) == Some(x),
+                },
+                contract! {
+                    header: "Associativity",
+                    inputs: [x: $typ, y: $typ, z: $typ],
+                    precondition: true,
+                    postcondition: x.checked_add(y).and_then(|iv| iv.checked_add(z)) == y.checked_add(z).and_then(|iv| x.checked_add(iv)),
+                },
+            }
+        }
+    };
+}
 
 pub fn specs() {
     tests! {
@@ -33,44 +124,9 @@ pub fn specs() {
             postcondition: panics!(x + y),
         },
     }
-    tests! {
-        header: "Properties for [`core::ops::Add::<u8>::add`]",
-        ident: core_ops_add_u8_add,
-        contract! {
-            header: "Semantics of non-overflowing addition",
-            inputs: [x: u8, y: u8],
-            precondition: x.up() + y.up() < 256.up(),
-            postcondition: x + y == eval(u8::down(x.up() + y.up())),
-        },
-        contract! {
-            header: "Panics when overflowing",
-            inputs: [x: u8, y: u8],
-            precondition: x.up() + y.up() >= 256.up(),
-            postcondition: panics!(x + y),
-        },
-        contract! {
-            header: "Commutativity",
-            inputs: [x: u8, y: u8],
-            precondition: x.up() + y.up() < 256.up(),
-            postcondition: x + y == y + x,
-        },
-        contract! {
-            header: "Left identity",
-            inputs: [x: u8],
-            precondition: true,
-            postcondition: x + 0 == x,
-        },
-        contract! {
-            header: "Right identity",
-            inputs: [x: u8],
-            precondition: true,
-            postcondition: 0 + x == x,
-        },
-        contract! {
-            header: "Associativity",
-            inputs: [x: u8, y: u8, z: u8],
-            precondition: x.up() + y.up() + z.up() < 256.up(),
-            postcondition: (x + y) + z == x + (y + z),
-        },
-    }
+    mk_unsigned_tests!(u8, core_ops_add_u8_add);
+    mk_unsigned_tests!(u16, core_ops_add_u16_add);
+    mk_unsigned_tests!(u32, core_ops_add_u32_add);
+    mk_unsigned_tests!(u64, core_ops_add_u64_add);
+    mk_unsigned_tests!(u128, core_ops_add_u128_add);
 }
