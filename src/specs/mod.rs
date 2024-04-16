8af68f416e07c202c579b26c651162c975749905
Lucas Franceschino <lucas@cryspen.com>
2024-04-16 09:33:23 +0200

    chore: remove dead code

--- src/specs/mod.rs
+++ src/specs/mod.rs
@@ -6,27 +6,4 @@ pub fn specs() {
     option::specs();
     vec::specs();
     num::specs();
-
-    // tests! {
-    //     header: "Function `foo`",
-    //     ident: wrapping_add,
-    //     contract! {
-    //         header: "Property `bar`",
-    //         inputs: [x: u8],
-    //         precondition: x < 20,
-    //         postcondition: eval(x + 20) == x + 20,
-    //         test_vector: [0, 1, 2]
-    //     }
-    // }
-
-    // tests! {
-    //     header: "Function `foo`",
-    //     ident: other,
-    //     contract! {
-    //         header: "Property `bar`",
-    //         inputs: [x: u8, y: u8],
-    //         precondition: x < 10 && y < 20,
-    //         postcondition: eval(x + 20 + y) == y + x + 20,
-    //     }
-    // }
 }
